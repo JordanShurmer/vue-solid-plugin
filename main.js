ab99387d5c5b085ede6008493ba7def7efbd5aaa
Jordan Shurmer <jshurmer@scrippsnetworks.com>
2019-06-05 10:39:51 -0400

    Add setimmediate polyfill

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // ESM syntax is supported.
 import SolidLogin from './solid-login.js';
 import Solid from './vue-plugin.js';
+import 'setimmediate'; //adds setImmediate polyfil :\
  
 export {
     Solid,
